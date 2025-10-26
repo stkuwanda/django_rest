cf21ff508b559bec3a6cf741d7505ff3248db92e
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-10-26 14:28:24 +0200

    create and register views with urlpatterns to create api end-points

---
+++ crud_api/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import get_user
+
+# define urlpatterns for the api app
+# these urlpatterns will route requests to the get_user view.
+# a view is a function that takes a web request and returns a web response. In NodeJs views are called controllers.
+urlpatterns = [path('users/', get_user, name='get_user')] 
--- crud_api/api/views.py
+++ crud_api/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_user(request):
+  return Response(UserSerializer({"name":"Michak-nav", "age": "0"}))
