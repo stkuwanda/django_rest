7a4b004565af158cba799ddb6a2f44b41ac02dd7
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-10-26 17:35:18 +0200

    fix serializer instance creation bug

--- crud_api/api/views.py
+++ crud_api/api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from .models import User
 from .serializer import UserSerializer
 
+ 
 @api_view(['GET'])
-def get_user(request):
-  return Response(UserSerializer({"name":"Michak-nav", "age": "0"}))
+def get_user(request): 
+  serializer = UserSerializer({"name":"Michal-nav", "age": 0}) # create a UserSerializer instance with sample data
+  return Response(serializer.data) # return the serialized data as a response
