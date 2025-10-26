fff89d118b760f6a807bed297f54670eced73ef5
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-10-26 18:17:45 +0200

    create and register create_user api end-point

--- crud_api/api/urls.py
+++ crud_api/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import get_user
+from .views import get_user, create_user
 
 # define urlpatterns for the api app
 # these urlpatterns will route requests to the get_user view.
 # a view is a function that takes a web request and returns a web response. In NodeJs views are called controllers.
-urlpatterns = [path('users/', get_user, name='get_user')] 
+urlpatterns = [
+  path('users/', get_user, name='get_user'), # route to get_user view
+  path('users/create', create_user, name='create_user') # route to create_user view
+] 
--- crud_api/api/views.py
+++ crud_api/api/views.py
@@ -9,3 +9,11 @@ from .serializer import UserSerializer
 def get_user(request): 
   serializer = UserSerializer({"name":"Michal-nav", "age": 0}) # create a UserSerializer instance with sample data
   return Response(serializer.data) # return the serialized data as a response
+
+@api_view(['POST'])
+def create_user(request): 
+  serializer = UserSerializer(data=request.data) # create a UserSerializer instance with the data from the request
+  if serializer.is_valid(): # check if the data is valid
+    serializer.save() # save the new user to the database
+    return Response(serializer.data, status=status.HTTP_201_CREATED) # return the serialized data with a 201 status code
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # return the errors with a 400 status code
