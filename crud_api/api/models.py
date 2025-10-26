53b60cde3b82a356f846317dd75c2aead7fd9782
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-10-26 13:47:41 +0200

    create models, make and run migrations based on models

--- crud_api/api/models.py
+++ crud_api/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  age = models.IntegerField()
+  name = models.CharField(max_length=100)
+
+  # String representation of the User model through __str__ method with name field
+  # The self parameter refers to the current instance of the User model
+  # and the __str__ is the name of the method that return a string representation of the object.
+  def __str__(self):
+    return self.name
