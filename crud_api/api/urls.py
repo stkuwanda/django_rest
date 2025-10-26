dd81c503dc04336fb73bb41d14eb0e0d370488c3
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-10-27 00:07:53 +0200

    create and register user-details api end-point

--- crud_api/api/urls.py
+++ crud_api/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import get_users, create_user
+from .views import get_users, create_user, user_detail
 
 # define urlpatterns for the api app
 # these urlpatterns will route requests to the get_user view.
 # a view is a function that takes a web request and returns a web response. In NodeJs views are called controllers.
 urlpatterns = [
   path('users/', get_users, name='get_user'), # route to get_user view
-  path('users/create', create_user, name='create_user') # route to create_user view
+  path('users/create', create_user, name='create_user'), # route to create_user view
+  path('users/<int:pk>', user_detail, name='user_detail') # route to user_detail view
 ] 
--- crud_api/api/views.py
+++ crud_api/api/views.py
@@ -9,7 +9,7 @@ from .serializer import UserSerializer
 def get_users(request): 
   users = User.objects.all()[:10] # retrieve 10 users
   serializer = UserSerializer(users, many=True) # serialize the users which can be a list (many=true)
-  return Response(serializer.data)
+  return Response(serializer.data) # return serialized data
  
 @api_view(['POST'])
 def create_user(request): 
@@ -18,3 +18,22 @@ def create_user(request):
     serializer.save() # save the new user to the database
     return Response(serializer.data, status=status.HTTP_201_CREATED) # return the serialized data with a 201 status code
   return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # return the errors with a 400 status code
+
+@api_view(['GET','PUT','DELETE'])
+def user_detail(request, pk):
+  try:
+    user = User.objects.get(pk=pk) # retrieve the user by primary key
+  except User.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND) # return 404 if user not found
+  if request.method == 'GET':
+    serializer = UserSerializer(user) # serialize the user
+    return Response(serializer.data) # return the serialized data
+  elif request.method == 'PUT':
+    serializer = UserSerializer(user, data=request.data) # create a UserSerializer instance with the existing user and the new data
+    if serializer.is_valid(): # check if the new daata is valid
+      serializer.save() # save the updated user to the database
+      return Response(serializer.data) # return the serialized data
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # return the errors with a 400 status code
+  elif request.method == 'DELETE':
+    user.delete() # delete the user from the database
+    return Response(status=status.HTTP_204_NO_CONTENT) # return 204 status code indicating successful deletion
